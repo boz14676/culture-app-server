c321c67bebb893c2413a7ae553a9fc6ba0f5e108
张博 <zhangbo@geek-zoo.com>
2017-07-27 15:48:31 +0800

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ $app->group(['namespace' => 'App\Http\Controllers\v1', 'middleware' => ['xss']],
 // Api - Guest
 $app->group(['namespace' => 'App\Http\Controllers\v1','prefix' => 'v1', 'middleware' => ['xss']], function($app)
 {
-    $app->get('article_categories', 'ArticleController@i000000'); // 获取文章类别(s)
+    $app->get('article_categories', 'ArticleController@i0000'); // 获取文章类别(s)
     $app->get('articles', 'ArticleController@_lists'); // 获取文章(s)
 });
 
