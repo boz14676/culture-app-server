5ffabe7ac29bb1fb9512972217a502388b402e10
张博 <zhangbo@geek-zoo.com>
2017-07-27 18:59:50 +0800

    no message

--- public/hook/post-commit.php
+++ public/hook/post-commit.php
@@ -1,20 +1,4 @@
 <?php
 
-echo exec("cd /pro/pzjhw/appapi.pzjhw.com && sudo bash .git/hooks/post-receive", $file);
-echo "</br>";
-print_r($file);
-
-/*$log = '';
-$log .= '---------- SYNC'.PHP_EOL;
-// $command = str_replace(array_keys($git), array_values($git), __CMD_SYNC__);
-$command = 'cd /pro/pzjhw/appapi.pzjhw.com && mkdir test123';
-echo '<hr/>EXECUTE COMMAND: '.$command.'<br/>';
-$log .= 'Executing: '.$command.PHP_EOL;
-
-
-
-shell_exec($command, $result).'<hr/>';
-echo '* '.implode('<br/>* ', $result);
-$log .= 'Result: '.PHP_EOL.'* '.implode(PHP_EOL.'* ', $result).PHP_EOL.PHP_EOL;*/
-
+echo exec("cd /pro/pzjhw/appapi.pzjhw.com && sudo bash .git/hooks/post-receive");
 ?>
